96a4660250b84236943f7d22bb58608a975d7eac
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2012-09-18 14:34:37 +0200

    fix base emv

--- lib/emailvision/base.rb
+++ lib/emailvision/base.rb
@@ -17,13 +17,13 @@ module DriesS
         self.email_column =  opts[:email] || 'email'
 
         class_attribute :confirmed_column
-        self.confirmed_column =  opts[:confirmed]
+        self.confirmed_column = opts[:confirmed]
 
         class_attribute :emailvision_enabled_column
         self.emailvision_enabled_column = opts[:enabled]
 
         class_attribute :emailvision_merge_vars
-        self.emailvision_merge_vars = MergeVars.new(&block)
+        self.emailvision_merge_vars = MergeVars.new(&block) if opts[:peform_block]
 
         self.send(:include, EmailvisionSubscriber)
       end

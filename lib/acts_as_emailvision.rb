5139f0d10a5a144eed15ff564a7cd7ae5cadfbc0
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2013-02-13 10:56:40 +0100

    fix data format

--- lib/acts_as_emailvision.rb
+++ lib/acts_as_emailvision.rb
@@ -9,4 +9,20 @@ if defined?(Rails)
   require 'acts_as_emailvision/railtie'
 end
 
+::Emailvision::Tools.class_eval do 
+  
+  def self.date_time_format(datetime)
+    datetime.strftime('%m/%d/%Y')
+  end
+
+  def self.date_format(datetime)
+    datetime.strftime('%m/%d/%Y')
+  end
+
+  def self.date_format_unsubscribe(datetime)
+    datetime.strftime('%m/%d/%Y')
+  end
+
+end
+
 ActiveRecord::Base.send(:include, DriesS::ActsAsEmailvision) # trigger included method in emailvision_subscriber.rb
